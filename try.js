f1b7c8b2be674f95d38a460a118c2bc36d79beb5
Sowmayjain <champ.sowmay@gmail.com>
2019-07-06 21:55:34 +0530

    Fixed JS error.

--- try.js
+++ try.js
@@ -1,14 +1,24 @@
-setTimeout(start, 5000);
+////// Previous Code with core Javascript
 
-var i = 1;
-var num = document.getElementsByClassName("counter");
+// setTimeout(start, 5000);
 
-function start() {
-    setInterval(increase, 1000);
-}
-function increase() {
-    if(i < 100) {
-        i++;
-        num.innerText = i;
-    }
-}
+// var i = 1;
+// var num = document.getElementsByClassName("counter");
+
+// function start() {
+//     setInterval(increase, 1000);
+// }
+// function increase() {
+//     if(i < 100) {
+//         i++;
+//         num.innerText = i;
+//     }
+// }
+
+////// New Code with JQuery
+var i = 0;
+setInterval(() => {
+    console.log(i);
+    i++;
+    $(".counter").text(i);
+}, 1000);
